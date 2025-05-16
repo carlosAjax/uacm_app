536641ea503e88043d3fd2055cd85aafd8003686
carlosAjax <carlos.ramirez.garcia@estudiante.uacm.edu.mx>
2025-05-16 01:31:41 -0600

    chore: inicializar Firebase en main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:uacm_app/screens/calendario_escolar_screen.dart';
@@ -11,6 +12,8 @@ import 'package:uacm_app/util/util.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   // Asegura que las barras de estado y navegación estén visibles
   /* SystemChrome.setEnabledSystemUIMode(
     SystemUiMode.manual,
